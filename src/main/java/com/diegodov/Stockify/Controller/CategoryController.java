c03242c95b483fe9a5bc035487e9a9ebd3222923
Diego Dominguez <diegodd07@gmail.com>
2023-10-20 00:04:11 -0300

    Endpoint edit bug fixed

--- src/main/java/com/diegodov/Stockify/Controller/CategoryController.java
+++ src/main/java/com/diegodov/Stockify/Controller/CategoryController.java
@@ -5,14 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.diegodov.Stockify.Model.Category;
 import com.diegodov.Stockify.Service.CategoryService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/views/categories")
@@ -47,12 +51,13 @@ public class CategoryController {
         return "redirect:/views/categories/";
     }
 
-    @GetMapping("/edit/{id}")
-    public String edit(@PathVariable("id") Long id, Model model) {
+    @GetMapping("/details/{id}")
+    public String details(@PathVariable("id") Long id, Model model) {
         Category category = categoryService.showDetails(id);
         model.addAttribute("title", "Modify category");
+        model.addAttribute("id", id);
         model.addAttribute("category", category);
-        return "categoryform";
+        return "editcategory";
     }
 
     @GetMapping("/delete/{id}")
@@ -61,6 +66,15 @@ public class CategoryController {
         System.out.println("Registro eliminado");
         return "redirect:/views/categories/";
     }
-    
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable("id") long id, @Valid Category category, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            category.setId(id);
+            return "update-user";
+        }
+        categoryService.save(category);
+        return "redirect:/views/categories/";
+    }
 
 }
--- src/main/java/com/diegodov/Stockify/Service/CategoryService.java
+++ src/main/java/com/diegodov/Stockify/Service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.diegodov.Stockify.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,7 @@ public class CategoryService {
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
-
+    
     //save category
     public void save(Category category){
         categoryRepository.save(category);
@@ -36,6 +37,20 @@ public class CategoryService {
     //show details by id
     public Category showDetails(Long id){
         return categoryRepository.findById(id).orElse(null);
+    }
+
+    //update category
+    public boolean updateCategoryName(Long id, String newCategoryName) {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        if (categoryOptional.isPresent()) {
+            Category category = categoryOptional.get();
+            category.setName(newCategoryName);
+            categoryRepository.save(category); // Guarda la categoría actualizada en la base de datos
+            return true;
+        }
+
+        return false; // Devuelve false si la categoría no se encontró o no se pudo actualizar
     }
 
 }
